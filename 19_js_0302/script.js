b9de1ce4659908d115216b4f64d36a4075bf1637
demidraksin <greatraksin@icloud.com>
2023-02-03 20:27:05 +0300

    Adding js functions

--- 19_js_0302/script.js
+++ 19_js_0302/script.js
@@ -3,7 +3,19 @@
 
 function change_background(color) {  // функция получает цвет в качестве параметра
     let body = document.querySelector('body');
-    body.style.background = color;
+    let bg = window.getComputedStyle(body).backgroundColor;
+    // в переменной сохраняю цвет, который применен к фону страницы
+    if (bg == 'rgba(0, 0, 0, 0)') {  // если цвет фона страницы белый
+        body.style.background = color;  // поменять его на тот цвет, который передали функции
+    } else {  // иначе (если цвет фона НЕ белый)
+        body.style.background = 'rgba(0, 0, 0, 0)';  // поменять его на белый
+    }
+
+}
+
+function change_font(id) {  // функция получается id в качестве параметра
+    let element = document.getElementById(id);
+    element.classList.add('changed');
 }
 
 
