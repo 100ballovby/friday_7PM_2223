b044fa965329955f2c063716598ce701eb925c60
demidraksin <greatraksin@icloud.com>
2023-02-17 20:20:26 +0300

    Adding js forms

--- 21_формы_js_1702/script.js
+++ 21_формы_js_1702/script.js
@@ -6,3 +6,13 @@ function btnPress() {
     let div = document.querySelector('.ex1');
     div.innerHTML = "<hr>" + "Вы нажали кнопку " + txt1 + " с именем: " + txt2 + "<hr>";
 }
+
+function info() {
+    let sI = document.Test.list1.selectedIndex;  // сохраняю индекс выбранного элемента в селекте
+    let txt;
+    txt = "Предложено " + document.Test.list1.length + " напитков." +
+        "\nВыбран " + document.Test.list1.options[sI].text +
+        " индекс элемента: " + document.Test.list1.options[sI].defaultSelected;
+
+    alert(txt);
+}
