7b9df63bb501bacf52cad6695b24ca88e9ac9209
greatraksin <greatraksin@icloud.com>
2023-03-03 19:42:43 +0300

    adding caesar cipher

---
+++ 23_шифр_цезаря_2502/script.js
@@ -0,0 +1,23 @@
+let ru = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+let eng = "abcdefghijklmnopqrstuvwxyz";
+
+function caesar_cipher(phrase, shift) {
+    let res = "";
+    for (let i = 0; i < phrase.length; i++) {
+        let place = ru.indexOf(phrase[i]);  // определяем место буквы в алфавит
+        let new_place = place + shift;
+
+        if (new_place > ru.length) {  // если вышли за пределы алфавита
+            new_place -= ru.length;  // отнять длину алфавита от нового индекса буквы
+        }
+
+        if (ru.includes(phrase[i])) {  // если символ есть в алфавите
+            res += ru[new_place];
+        } else {
+            res += phrase[i];
+        }
+    }
+    let p = document.getElementById('res');
+    p.innerText = res;
+}
+
