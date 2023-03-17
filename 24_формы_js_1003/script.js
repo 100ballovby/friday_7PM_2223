d7cdf188b99f3834eac11e7010b3cd29485d6a8c
greatraksin <greatraksin@icloud.com>
2023-03-17 20:21:15 +0300

    Adding mars one form

--- 24_формы_js_1003/script.js
+++ 24_формы_js_1003/script.js
@@ -1,11 +1,30 @@
 function serialize_form(form) {
-    console.log(form.elements);
+    const { elements } = form;  // сохраняем элементы формы в виде объекта
+    const data = Array.from(elements).filter((item) => !!item.name).map((element) => {
+        const { name, value } = element;
+        return { name, value };
+    });
+
+    console.log(data);
 }
 
-function form_submit(event) {
-    event.preventDefault();
-    serialize_form(form);
+function serialize(form) {
+    return new FormData(form);
 }
+
+async function form_submit(event) {
+    event.preventDefault();
+    const data = serialize(event.target);  // сохраним массив с данными из формы
+    const response = await sendData(data);
+}
+
+async function sendData(data) {
+    return await fetch('/api/apply/', {
+        'method': 'POST',
+        'headers': {'Content-Type': 'multipart/form-data'},
+        'body': data,
+    })
+}  // функция отправки данных на "сервер"
 
 
 const form = document.getElementById('mars-one');
